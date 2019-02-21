29e80bdffb50d37a2505ae4f3b5e87b39f813352
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-21 04:47:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,22 @@
-class Song
-
-  attr_accessor :name, :artist
-
+class Song 
+  attr_accessor :name, :artist, :new_by_filename
+  
   def initialize(name)
-    @name=name
+    @name = name
   end
-
-  def self.new_by_filename(file)
-    song_name = file.split("-")[1]
-    artist = file.split("-")[0]
-    song = self.new(song_name)
-    song.artist_name = artist
+  
+  def self.new_by_filename (filename)
+    file = filename.split(" - ")
+    song_name = file[1]
+    artist_name = file[0]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
     song
-  end
-
+  end 
+  
   def artist_name=(name)
-    self.artist= Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
-  end
-end
+  end 
+  
+end 
